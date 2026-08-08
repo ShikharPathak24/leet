e50d26e88ce62cf6c2f43b529b0fe904dad066bb
Shikhar Pathak <shikharpathak24@gmail.com>
2026-08-08 20:19:20 +0530

    Time: 2 ms (96.88%) | Memory: 43.4 MB (97.62%) - LeetSync

--- 415-add-strings/add-strings.java
+++ 415-add-strings/add-strings.java
@@ -4,10 +4,11 @@ class Solution {
         int j = num2.length() - 1;
         int carry = 0;
         StringBuilder sb = new StringBuilder();
+        int sum = 0;
         while (i >= 0 || j >= 0 || carry > 0){
             int digit1 = (i >= 0) ? num1.charAt(i) - '0' : 0;
             int digit2 = (j >= 0) ? num2.charAt(j) - '0' : 0;
-            int sum = 0;
+            
             sum = digit1 + digit2 + carry;
             sb.append(sum % 10);
             carry = sum / 10;
