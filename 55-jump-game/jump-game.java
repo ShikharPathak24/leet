ff59fb0f99a247878b90f54307a8ba7a2d57119a
Shikhar Pathak <shikharpathak24@gmail.com>
2026-09-06 17:20:30 +0530

    Time: 2 ms (89.05%) | Memory: 48.2 MB (8.63%) - LeetSync

---
+++ 55-jump-game/jump-game.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+
+        int farthest = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            // Current index is unreachable
+            if (i > farthest) {
+                return false;
+            }
+
+            // Update farthest reachable index
+            farthest = Math.max(farthest, i + nums[i]);
+
+            // Already reached the end
+            if (farthest >= nums.length - 1) {
+                return true;
+            }
+        }
+
+        return true;
+    }
+}
